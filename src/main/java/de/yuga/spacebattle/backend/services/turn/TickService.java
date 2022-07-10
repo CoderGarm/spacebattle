113cfd7d889e87ca1a93697aca2d013a20ec6ccd
CoderGarm <kar.lie@gmx.de>
2022-07-10 18:43:24 +0200

    run tick at 24:00 every day

--- src/main/java/de/yuga/spacebattle/backend/services/turn/TickService.java
+++ src/main/java/de/yuga/spacebattle/backend/services/turn/TickService.java
@@ -30,6 +30,7 @@ import de.yuga.spacebattle.rest.api.error.NotifyWebUserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -122,7 +123,7 @@ public class TickService {
         this.battleService = battleService;
     }
 
-    //@Scheduled(cron = "* */1 * * * *")
+    @Scheduled(cron = "0 0 24 * * *", zone = "Europe/Berlin")
     private void doIt() {
         LOGGER.info("Tick scheduled");
         this.doTick();
