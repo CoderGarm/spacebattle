257d7b58e9a85bee7deba1f31efb5e7d9e602619
CoderGarm <kar.lie@gmx.de>
2024-01-24 11:22:15 +0100

    upgrade pom and introduce orekit

---
+++ src/test/java/de/yuga/spacebattle/backend/calculator/distance/NavigationCalculatorTest.java
@@ -0,0 +1,14 @@
+package de.yuga.spacebattle.backend.calculator.distance;
+
+import org.junit.jupiter.api.Test;
+
+class NavigationCalculatorTest {
+
+
+    @Test
+    void thrustManeuvers() {
+
+
+    }
+
+}
